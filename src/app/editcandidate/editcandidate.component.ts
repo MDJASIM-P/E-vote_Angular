014cf0fc20f2cf05c2e01d06c4b7e4da18f437b5
MDJASIM-P <mdjasimp0305@gmail.com>
2023-09-17 10:07:30 +0530

    messege

---
+++ src/app/editcandidate/editcandidate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editcandidate',
+  templateUrl: './editcandidate.component.html',
+  styleUrls: ['./editcandidate.component.css']
+})
+export class EditcandidateComponent {
+
+}
